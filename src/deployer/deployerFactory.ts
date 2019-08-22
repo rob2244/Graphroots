a9dc913d73db029a6811f8911923bddb669edcc2
Robin Seitz <robinseitz@hotmail.com>
2019-08-22 15:52:01 -0700

    403 | Update the deployment factory to return an AWS deployer when a deployer type of AWS is specified

--- src/deployer/deployerFactory.ts
+++ src/deployer/deployerFactory.ts
@@ -1,18 +1,22 @@
-import DeployerType from "./deployerType";
-import IDeployer from "./deployer";
-import AzureDeployer from "./azure/azureDeployer";
+import DeployerType from './deployerType';
+import IDeployer from './deployer';
+import AzureDeployer from './azure/azureDeployer';
+import AWSDeployer from './aws/AWSDeployer';
 
 export default function createDeployer(
-  type: DeployerType,
-  context: any
+	type: DeployerType,
+	context: any
 ): IDeployer {
-  switch (type) {
-    case DeployerType.Azure:
-      return new AzureDeployer(context);
+	switch (type) {
+		case DeployerType.Azure:
+			return new AzureDeployer(context);
 
-    default:
-      throw new Error(`Unrecognized deployer type: ${type}`);
-  }
+		case DeployerType.AWS:
+			return new AWSDeployer(context);
+
+		default:
+			throw new Error(`Unrecognized deployer type: ${type}`);
+	}
 }
 
 export type createDeployer = (type: DeployerType, context: any) => IDeployer;
